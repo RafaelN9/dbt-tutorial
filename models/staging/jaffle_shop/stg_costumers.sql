398689f31f15dfb489a18893ae7aac8f95ef8cdc
Rafael Correia <rcorreia@buildaz.io>
2022-12-13 16:42:45 +0000

    Sources + tests

--- models/staging/jaffle_shop/stg_costumers.sql
+++
@@ -1,8 +0,0 @@
-with
-    customers as (
-
-        select id as customer_id, first_name, last_name from `dbt-learning-371413.jaffle_shop_1.costumers`
-
-    )
-select *
-from jaffle_shop_1.costumers;
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,10 +1,9 @@
-with
-    orders as (
-
-        select id as order_id, user_id as customer_id, order_date, status
-
-        from `dbt-learning-371413.jaffle_shop_1.orders`
-
-    )
-select *
-from jaffle_shop_1.orders;
+with orders as (
+    select 
+        id as order_id, 
+        user_id as customer_id, 
+        order_date, 
+        status
+    from {{ source('jaffle_shop', 'orders') }}
+)
+select * from orders
